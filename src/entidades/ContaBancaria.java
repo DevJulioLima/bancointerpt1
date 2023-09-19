d255edb94dc22b9ad32600bb68c9d1e292fdfa01
DevJulioLima <julioempresarioexecultivo@gmail.com>
2023-09-19 12:00:30 -0300

    entidade ContaBancaria criada

---
+++ src/entidades/ContaBancaria.java
@@ -0,0 +1,47 @@
+package entidades;
+
+public class ContaBancaria {
+    private String numeroConta;
+    private float saldo;
+    private String agencia;
+    private float extrato;
+
+    public ContaBancaria(String numeroConta, float saldo, String agencia, float extrato) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+        this.agencia = agencia;
+        this.extrato = extrato;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public float getExtrato() {
+        return extrato;
+    }
+
+    public void setExtrato(float extrato) {
+        this.extrato = extrato;
+    }
+}
