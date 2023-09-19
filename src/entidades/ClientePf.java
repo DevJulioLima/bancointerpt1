18d2ef5221db00ed455f669e4d53b5dc899ca8ae
DevJulioLima <julioempresarioexecultivo@gmail.com>
2023-09-19 11:52:23 -0300

    entidade ClientePf criada

---
+++ src/entidades/ClientePf.java
@@ -0,0 +1,23 @@
+package entidades;
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class ClientePf extends Pessoa{
+    private float rendaMensal;
+
+    private ArrayList<ClientePf> clientePf = new ArrayList<>();
+
+    public ClientePf(int id, boolean tipoCliente, String nome, String email, Date dataDeNascimento, String telefone, String rg, String cpf) {
+        super(id, tipoCliente, nome, email, dataDeNascimento, telefone, rg, cpf);
+
+        this.rendaMensal = rendaMensal;
+    }
+
+    public float getRendaMensal() {
+        return rendaMensal;
+    }
+
+    public void setRendaMensal(float rendaMensal) {
+        this.rendaMensal = rendaMensal;
+    }
+}
